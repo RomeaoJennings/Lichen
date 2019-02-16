0ec6f160785081f0a20eff8cdc8430534e987d0a
Romeao Jennings <romeao.jennings@gmail.com>
2019-02-16 13:30:21 -0800

    Add zobrist hash structure

---
+++ Typhoon/Model/ZobristHash.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Typhoon.Model
+{
+    public static class ZobristHash
+    {
+        private static readonly Random random = new Random(8675309);
+
+        // Indexed by color, piece, square
+        private static readonly ulong[][][] pieces = InitPieces();
+
+
+
+        private static ulong[][][] InitPieces()
+        {
+            ulong[][][] result = new ulong[2][][];
+
+            for (int color = 0; color < 2; color++)
+            {
+                result[color] = new ulong[6][];
+                for (int piece = 0; piece < 6; piece++)
+                {
+                    result[color][piece] = new ulong[64];
+                    for (int square = 0; square < 64; square++)
+                    {
+                        ulong upperBits = (ulong)random.Next();
+                        upperBits <<= 32;
+|= (ulong)random.Next();
+                        result[color][piece][square] = upperBits;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
